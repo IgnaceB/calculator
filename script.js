82f488b452e0a271588ffe9b39b8d2106af7858d
IgnaceB <ignacebernard@hotmail.com>
2023-06-09 17:08:20 +0200

    operator sci

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //creating sections
 const calculator = document.querySelector("main")
-const log=document.createElement("ul")
+const log = document.createElement("ul")
 document.body.appendChild(log)
 
 const createSecNb = document.createElement("section")
@@ -11,13 +11,12 @@ calculator.appendChild(createSecOp)
 calculator.appendChild(createScreen)
 
 const SecNb = calculator.lastElementChild
-const SecOp = calculator.children[1] 
+const SecOp = calculator.children[1]
 const SecScreen = calculator.firstElementChild
 
 //styling sections
-document.style="box-sizing:border-box"
-calculator.style = "width:350px; height:400px; border: solid 2px black;"
-SecScreen.style = "display:flex; height:110px; width:100%; justify-content:space-between; align-items:center; flex-direction: column;margin-bottom:10px;border:solid black 1px"
+calculator.style = "width:350px; height:400px; border: solid 2px black;background: aquamarine;"
+SecScreen.style = "height:120px; width:100%; margin-bottom:10px;border:solid black 1px;background: aliceblue;"
 SecNb.style = "box-sizing:border-box;display:flex; height:200px; width:100% ; justify-content:flex-start; align-items:space-between; flex-wrap:wrap; padding:10px;"
 SecOp.style = "display:flex; height:50px; width:100%; justify-content:space-around; align-items:center"
 
@@ -28,12 +27,12 @@ SecScreen.appendChild(history)
 SecScreen.appendChild(text)
 const historyscreen = SecScreen.firstElementChild
 const screen = SecScreen.lastElementChild
-screen.style = "width:300px; height:100px; border:solid 1px;"
-historyscreen.style = "width:300px; height:100px; border:solid 1px;"
+screen.style = "width:90%; height:40px; border:solid 1px;margin-left:15px;overflow:scroll"
+historyscreen.style = "width:90%; height:40px; border:solid 1px;margin-left:15px;overflow:scroll"
 
 // Styles
-const styleNum = "margin:10px;height:50px; width:70px; border: solid 1px; margin: 5px;"
-const styleOp = "height:40px; width:40px; border: solid 1px; margin: 1px;"
+const styleNum = "background: pink;margin:10px;height:50px; width:70px; border: solid 1px; margin: 5px;"
+const styleOp = "height:40px; width:40px; border: solid 1px; margin: 1px;background: bisque;"
 const stylep = "height:50px; width:70px; margin:0;padding:0; text-align:center; padding-top:5px; cursor:pointer"
 const stylepOp = "height:40px; width:40px; margin:0;padding:0; text-align:center; padding-top:5px; cursor:pointer"
 const stylescreen = ""
@@ -50,6 +49,17 @@ const reportNb = (event) => {
 
     screen.innerText = screen.innerText + event.target.id
 
+
+}
+
+//event on click on Scientific operator (log-sin-cos)
+const reportSciOp = (event) => {
+    screen.innerText = screen.innerText + event.target.id
+    arrayRes.push(screen.innerText)
+    arrayOld=arrayOld+event.target.id
+    screen.innerText=""
+    historyscreen.innerText=arrayOld
+    countOp++
 }
 
 //event on click on operator
@@ -66,32 +76,53 @@ const reportOp = (event) => {
             if (total == 0) {
                 console.log("init")
                 console.log(total)
-                switch (arrayRes[index + 1]) {
-                    case "+":
+                if (arrayRes[index] == "Log" | arrayRes[index] == "sin" | arrayRes[index] == "cos") {
+                    switch (arrayRes[index]) {
+                        case "Log":
+                        console.log("CA RENTRE")
+                            total = Math.log(parseInt(arrayRes[index+1]))
+                            index = index + 2
+                            break
+                        case "sin":
 
-                        total = parseInt(arrayRes[index]) + parseInt(arrayRes[index + 2])
-                        index = index + 3
-                        break
-                    case "-":
+                        total = Math.sin(parseInt(arrayRes[index+1]))
+                        index = index + 2
+                            break
+                        case "cos":
 
-                        total = arrayRes[index] - arrayRes[index + 2]
-                        index = index + 3
-                        break
-                    case "x":
+                        total = Math.cos(parseInt(arrayRes[index+1]))
+                        index = index + 2
+                            break
+                }}
+                else {
+                    switch (arrayRes[index + 1]) {
+                        case "+":
 
-                        total = arrayRes[index] * arrayRes[index + 2]
-                        index = index + 3
-                        break
-                    case "/":
+                            total = parseInt(arrayRes[index]) + parseInt(arrayRes[index + 2])
+                            index = index + 3
+                            break
+                        case "-":
 
-                        total = arrayRes[index] / arrayRes[index + 2]
-                        index = index + 3
-                        break
+                            total = arrayRes[index] - arrayRes[index + 2]
+                            index = index + 3
+                            break
+                        case "x":
 
+                            total = arrayRes[index] * arrayRes[index + 2]
+                            index = index + 3
+                            break
+                        case "/":
+
+                            total = arrayRes[index] / arrayRes[index + 2]
+                            index = index + 3
+                            break
+
+                    }
                 }
             }
 
             else {
+                
                 console.log(arrayRes[index])
                 switch (arrayRes[index]) {
                     case "+":
@@ -118,13 +149,12 @@ const reportOp = (event) => {
             }
         }
 
-        // arrayOld=arrayOld+"="
         screen.innerText = total
         arrayRes = []
         countOp = ""
-        const loglist=document.createElement("li")
+        const loglist = document.createElement("li")
         document.body.lastElementChild.appendChild(loglist)
-        loglist.innerText=arrayOld+total
+        loglist.innerText = arrayOld + total
     }
     else if (event.target.id == "c") {
         arrayRes = []
@@ -134,7 +164,6 @@ const reportOp = (event) => {
 
     }
     else {
-        // arrayOld=arrayOld+screen.innerText+event.target.id
         arrayRes.push(screen.innerText)
         arrayOld = arrayOld + screen.innerText
         screen.innerText = event.target.id
@@ -148,14 +177,13 @@ const reportOp = (event) => {
         screen.innerText = ""
         countOp++
     }
-    // arrayOld=arrayOld+screen.innerText
     historyscreen.innerText = arrayOld
     console.log(arrayRes)
     console.log(countOp)
 }
 
 //liste of values
-const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "x", "/", "=", "c"]
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "x", "/", "=", "c", "Log", "sin", "cos"]
 
 //creating buttons
 value.forEach(element => {
@@ -174,6 +202,18 @@ value.forEach(element => {
         button.addEventListener('click', reportNb)
 
     }
+    else if (element == "Log" | element == "sin" | element == "sin") {
+        console.log("not int")
+        button.id = element
+        button.className = "operator"
+        button.style = styleOp
+        SecOp.appendChild(button)
+        button.appendChild(text)
+        text.style = stylepOp
+        text.id = element
+        text.innerText = element
+        button.addEventListener('click', reportSciOp)
+    }
     else {
         console.log("not int")
         button.id = element
@@ -187,3 +227,9 @@ value.forEach(element => {
         button.addEventListener('click', reportOp)
     }
 })
+SecNb.appendChild(document.getElementById("c"))
+document.getElementById("c").style = styleNum + "background-color: bisque;"
+document.getElementById("c").firstElementChild.style = stylep
+SecNb.appendChild(document.getElementById("="))
+document.getElementById("=").style = styleNum + "background-color: bisque;"
+document.getElementById("=").firstElementChild.style = stylep
